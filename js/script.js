aab8562417bf5b6bad5a8f323dbe1e4091e41a0a
eraldo06 <eraldoleonardo384@gmail.com>
2022-11-21 19:28:44 -0300

    fazendo as variaveis da urna

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const s = (a)=>document.querySelector(a);
+
+let seuVotoPara = s('.d-1-1 span')
+let cargo = s('d-1-2 span')
+let descricao = s('.d-1-4')
+let aviso = s('.d2')
+let lateral = s('.d-1-right')
+let numeros = s('.d-1-3')
