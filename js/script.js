7bea5f8ad836eac539a254285a0d2b3ba49dd10c
eraldo06 <eraldoleonardo384@gmail.com>
2022-11-29 21:25:28 -0300

    erro, vou dormir

--- js/script.js
+++ js/script.js
@@ -49,7 +49,16 @@ function atualizarInterface() {
     if(candidato.length>0){
         candidato = candidato[0];
         seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block'
         descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+   
+        // colocando a foto
+        for(let i in candidato.fotos){
+        fotosHTML += `<div class="d-1-image"><img src="images/${candidato.fotos[i].url}" alt="" />${candidato.fotos[i].legenda}</div>`
+        }
+        let fotosHTML = '';
+
+        lateral.innerHTML = fotosHTML;
     }
 }
 
